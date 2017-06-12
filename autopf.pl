830759c775cf0c76f4effac0bf9e243d031d4fb0
lazaax <lazaax@lazaax-alienWare>
2017-06-11 20:24:34 -0500

    1.0

--- autopf.pl
+++ autopf.pl
@@ -1,21 +1,29 @@
 #!/usr/bin/perl
-# Probado en OPENBSD 5.8
-# Auto cargando pf
 #
+# Auto cargando pf para interface pppoe, funciona en OpenBSD 6.1
+# 
 
-$tarjeta = ´ppppoe0´
-
-$ip = `/sbin/ifconfig $tarjeta 2> /dev/null | grep inet | head -1 | awk '{ print \$2 }'` || die "arreglalo.\n";
-###$tes = `/bin/echo $ip > /dev/ttyC0`;
+$ip = `/sbin/ifconfig pppoe0 2> /dev/null | grep inet | head -1 | awk '{ 
+print \$2 }'` || die "if error arreglalo lazaax.\n";
+#$tes = `/bin/echo $ip > /dev/ttyC0`;
 
 $pf = `/sbin/pfctl -sr  | grep $ip`;
 if ($pf) {
 #        print "$tes";
 } else {
         print "ejecutando pf con nueva ip $ip\n";
+        system("/usr/bin/pkill tail");
+        system("/usr/bin/pkill tcpdump");       
         system("/sbin/pfctl -d");
         system("/sbin/pfctl -e");
         system("/sbin/pfctl -f /etc/pf.conf");
-        system("/bin/sleep 10");
+        system("/usr/sbin/tcpdump -o -N -nettt -vvvv -i pflog0 > /dev/ttyC0 &");
+        system("/usr/bin/tail -f /var/log/messages > /dev/ttyC0\n");
+        system("/usr/bin/tail -f /var/log/daemon > /dev/ttyC0\n");
+        system("/bin/echo sys7ipaddres:'$ip' > /dev/ttyC0\n");
+        system("/bin/echo sys7ipaddres:'$ip' > /dev/ttyC0\n");
+        system("/bin/echo sys7ipaddres:'$ip' > /dev/ttyC0\n");
+        system("/bin/echo sys7ipaddres:'$ip' > /dev/ttyC0\n");
 }
 
+#lzx
